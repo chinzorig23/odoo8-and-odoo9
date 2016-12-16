287a468f33163881c4ed9fb792c42cec4a5cc136
chinzorig23 <ivanchinzorig@gmail.com>
2016-12-16 16:43:40 +0800

    Odoo 8 block

---
+++ Cycle-Building-Block-8.0/Odoo-CMS-Cycle-Building-Block-8.0/website_cycle/__openerp__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Cycle Building Block',
+    'category': 'Website',
+    'summary': 'Image slider building block',
+    'version': '1.0',
+    'description': """
+Cycle Building Block
+======================================
+Image slider building block
+        """,
+    'author': '',
+    'website': '',
+    'depends': ['website'],
+    'data': [
+        'views/assets.xml',
+        'views/snippet.xml'
+    ],
+    'installable': True
+}
---
+++ Embed-HTML-Building-Block-8.0/Odoo-CMS-Embed-HTML-Building-Block-8.0/website_embed/__openerp__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Embed HTML Building Block',
+    'category': 'Website',
+    'summary': 'Allows to easily embed HTML into website',
+    'version': '1.0',
+    'description': """
+Embed HTML Building Block for Odoo CMS
+======================================
+This module appends the Odoo website builder building blocks collection with one more - HTML embedding building block.
+        """,
+    'author': '',
+    'website': '',
+    'depends': [
+        'website'
+    ],
+    'data': [
+        'views/assets.xml',
+        'views/snippet.xml'
+    ],
+    'installable': True
+}
